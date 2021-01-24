6cb5f0bb65ebaae8b7049b5f36fd490f9d563d84
adferhom <adferhom@inf.upv.es>
2021-01-25 00:55:59 +0100

    Creado proyecto java Spring

---
+++ SPRING/TFG-Patrones/src/main/java/me/ferrandis/TFGPatrones/TfgPatronesApplication.java
@@ -0,0 +1,13 @@
+package me.ferrandis.TFGPatrones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgPatronesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfgPatronesApplication.class, args);
+	}
+
+}
