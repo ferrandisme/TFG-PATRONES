ca039a6a1d8f982de4ab650b7ba8cb5f91f9f00e
adferhom <adferhom@inf.upv.es>
2021-02-19 21:01:15 +0100

    Encontrar Patron añadido a la navbar y añadida la misma a su html

---
+++ SPRING/TFG-Patrones/src/main/java/me/ferrandis/TFGPatrones/api/EncontrarPatronController.java
@@ -0,0 +1,17 @@
+package me.ferrandis.TFGPatrones.api;
+import me.ferrandis.TFGPatrones.modelo.Encapsulaciones.DatosVentanaPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EncontrarPatronController {
+
+
+    @GetMapping("/encontrarPatron")
+    public String informacionPrincipal(Model model) {
+        return "encontrar-patron";
+    }
+
+
+}
