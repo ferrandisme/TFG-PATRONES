78f54851934f2ee541a6994e3ab35b484196c430
adferhom <adferhom@inf.upv.es>
2021-02-21 18:46:04 +0100

    Creada estructura basica de IDs

--- SPRING/TFG-Patrones/src/main/java/me/ferrandis/TFGPatrones/api/EncontrarPatronController.java
+++ SPRING/TFG-Patrones/src/main/java/me/ferrandis/TFGPatrones/api/EncontrarPatronController.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Random;
+
 @Controller
 public class EncontrarPatronController {
 
     @GetMapping("/encontrarPatron")
     public String informacionPrincipal(Model model) {
+        Random rand = new Random();
+        long randomNum = rand.nextInt((Integer.MAX_VALUE));
+        model.addAttribute("iduser",randomNum);
         return "encontrar-patron";
     }
 }
--- SPRING/TFG-Patrones/src/main/java/me/ferrandis/TFGPatrones/api/TestEncontrarPatronController.java
+++ SPRING/TFG-Patrones/src/main/java/me/ferrandis/TFGPatrones/api/TestEncontrarPatronController.java
@@ -4,15 +4,30 @@ import me.ferrandis.TFGPatrones.modelo.Patron;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Controller
 public class TestEncontrarPatronController {
 
-    @GetMapping("/Encontrarpatron")
-    public String getPreguntaPatron(@RequestParam("id") String n, Model model) {
-        return "principal";
+
+
+    @GetMapping("/testEstructural/{id}")
+    public String iniciarEstructural(@PathVariable("id") String id , Model model) {
+        return "test-encontrar-patron";
+    }
+
+    @GetMapping("/testCreacional/{id}")
+    public String iniciarCreacional(@PathVariable("id") String id , Model model) {
+        return "test-encontrar-patron";
+    }
+
+    @GetMapping("/testComportamiento/{id}")
+    public String iniciarComportamiento(@PathVariable("id") String id , Model model) {
+        return "test-encontrar-patron";
     }
 }
