b70308c3d73895db6c3f5cd1561f5b7b80e6aa80
adferhom <adferhom@inf.upv.es>
2021-02-21 11:59:41 +0100

    Creado metodo getPreguntaPatron

--- SPRING/TFG-Patrones/src/main/java/me/ferrandis/TFGPatrones/api/TestEncontrarPatronController.java
+++ SPRING/TFG-Patrones/src/main/java/me/ferrandis/TFGPatrones/api/TestEncontrarPatronController.java
@@ -1,10 +1,19 @@
 package me.ferrandis.TFGPatrones.api;
 
+import me.ferrandis.TFGPatrones.modelo.Patron;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.UUID;
 
 @Controller
 public class TestEncontrarPatronController {
 
 
-    //VA A SER NECESARIO GESTIONAR LA SESION DEL USUARIO PARA IR PROCESANDO PASO A PASO EL TEST
+    @GetMapping("/Encontrarpatron")
+    public String getPreguntaPatron(@RequestParam("id") String n, Model model) {
+        return "principal";
+    }
 }
