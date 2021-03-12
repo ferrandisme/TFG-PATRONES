ef1cad6fba37097393c7b382f8dae0ad4a765809
adferhom <adferhom@inf.upv.es>
2021-03-12 11:49:39 +0100

    Refactorizada VentanaPrincipalController

--- SPRING/TFG-Patrones/src/main/java/me/ferrandis/TFGPatrones/api/VentanaPrincipalController.java
+++ SPRING/TFG-Patrones/src/main/java/me/ferrandis/TFGPatrones/api/VentanaPrincipalController.java
@@ -1,6 +1,7 @@
 package me.ferrandis.TFGPatrones.api;
 
 import me.ferrandis.TFGPatrones.DTO.DatosVentanaPrincipal;
+import me.ferrandis.TFGPatrones.servicio.PatronesServicio;
 import me.ferrandis.TFGPatrones.servicio.VentanaPrincipalServicioImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,14 +12,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class VentanaPrincipalController {
 
 
-    private final VentanaPrincipalServicioImp ventanaPrincipalServicio;
+    private final PatronesServicio patronesServicio;
 
     @Autowired
-    public VentanaPrincipalController(VentanaPrincipalServicioImp servicio){this.ventanaPrincipalServicio = servicio;}
+    public VentanaPrincipalController(PatronesServicio servicio){this.patronesServicio = servicio;}
 
     @GetMapping({"","/home","/","/index.html","/index"})
     public String informacionPrincipal(Model model) {
-        DatosVentanaPrincipal patrones = new DatosVentanaPrincipal(ventanaPrincipalServicio.getNumeroPatrones());
+        DatosVentanaPrincipal patrones = new DatosVentanaPrincipal(patronesServicio.getPatrones().size());
         model.addAttribute("patrones", patrones);
         return "principal";
     }
